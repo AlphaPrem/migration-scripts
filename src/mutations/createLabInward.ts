41f5e3ef781b8d0ad14aee6c55c87f7cbf0f947e
harsh.o <harsh.oza@alphabi.co>
2025-07-04 14:39:00 +0530

    2nd setup commit

---
+++ src/mutations/createLabInward.ts
@@ -0,0 +1,72 @@
+import axios,{ isAxiosError} from "axios"
+import { LabInwardInput } from "../types/labInward"
+
+async function createSeller(
+  labInwardInput: LabInwardInput
+): Promise<any> {
+  try {
+
+    const data = JSON.stringify({
+      operationName: "createB2BSealer",
+      query: `mutation createB2BSealer($input: SealerB2BInput) {
+         createB2BSealer(input: $input) {
+          id
+          name
+          tat
+          poc_name
+          poc_email
+          poc_phone
+          sealerId
+          isWhiteLabeled
+          email
+          website
+          logo
+          footerLogo
+          orgName
+          orgAddress
+          orgEmail
+          orgPhone
+          createdAt
+          updatedAt
+        }
+      }`,
+      variables: {
+        input: labInwardInput,
+      },
+    })
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: process.env.GRAPHQL_ENDPOINT_URL,
+      headers: {
+        "x-admin-token": process.env.GRAPHQL_TOKEN,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    }
+
+    const response = await axios.request(config)
+
+    if (!response.data.data || !response.data.data.createB2BSealer) {
+      throw new Error("Failed to create B2bClient In Lab database")
+    }
+
+    return response.data.data.createB2BSealer
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      console.dir(error?.response?.data)
+      throw new Error(
+        `Axios error occurred while creating seller: ${error?.response?.data?.errors?.[0]?.message}`
+      )
+    }
+    if (error instanceof Error) {
+      throw new Error(error.message)
+    }
+    throw new Error(`${error}`)
+  }
+}
+function isAxiosError(error: unknown) {
+    throw new Error("Function not implemented.")
+}
+

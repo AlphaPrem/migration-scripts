d5d8c29094b7bb18ca0123ea5917f05e585732f7
harsh.o <harsh.oza@alphabi.co>
2025-07-04 15:16:33 +0530

    fix:updateDNA and createDNA mutations

---
+++ src/mutations/DNA.ts
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { isAxiosError } from "axios";
+import { USERID } from "../../scripts/createLabprocesses";
+import logger from "../../lib/logger/logger";
+import { IDNAInput, IDNAUpdateInput } from "../types/dna";
+
+async function createDNA(
+  dnaInput: IDNAInput,
+  labProcessId: string
+): Promise<any> {
+  try {
+    const data = JSON.stringify({
+      operationName: "CreateDNA",
+      query: `mutation CreateDNA($labProcessId: String!, $input: DNAInput) {
+        createDNA(labProcessId: $labProcessId, input: $input) {
+          id
+        }
+      }
+      `,
+      variables: {
+        input: dnaInput,
+        labProcessId: labProcessId,
+      },
+    });
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: process.env.GRAPHQL_ENDPOINT_URL,
+      headers: {
+        "x-admin-token": process.env.GRAPHQL_TOKEN,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    const response = await axios.request(config);
+
+    if (!response.data.data || !response.data.data.createDNA) {
+      throw new Error("Failed to create DNA In Lab database");
+    }
+
+    return response.data.data.createDNA;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      console.dir(error?.response?.data);
+      logger.error(
+        `Axios error occurred while creating DNA: ${error?.response?.data?.errors?.[0]?.message}`
+      );
+      throw new Error(
+        `Axios error occurred while creating DNA: ${error?.response?.data?.errors?.[0]?.message}`
+      );
+    }
+    if (error instanceof Error) {
+      logger.error(error.message);
+      throw new Error(error.message);
+    }
+    logger.error(error);
+    throw new Error(`${error}`);
+  }
+}
+
+async function updateDNA(
+  updateDNAInput: IDNAUpdateInput,
+  updateDnaId: string
+): Promise<any> {
+  try {
+    const data = JSON.stringify({
+      operationName: "UpdateDNA",
+      query: `mutation UpdateDNA($updateDnaId: ID!, $input: DNAUpdateInput) {
+        updateDNA(id: $updateDnaId, input: $input)
+      }`,
+      variables: {
+        input: updateDNAInput,
+        updateDnaId: updateDnaId,
+      },
+    });
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: process.env.GRAPHQL_ENDPOINT_URL,
+      headers: {
+        "x-admin-token": process.env.GRAPHQL_TOKEN,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    const response = await axios.request(config);
+
+    if (!response.data.data || !response.data.data.UpdateDNA) {
+      throw new Error("Failed to update DNA In Lab database");
+    }
+
+    return response.data.data.UpdateDNA;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      console.dir(error?.response?.data);
+      logger.error(
+        `Axios error occurred while updating DNA: ${error?.response?.data?.errors?.[0]?.message}`
+      );
+      throw new Error(
+        `Axios error occurred while updating DNA: ${error?.response?.data?.errors?.[0]?.message}`
+      );
+    }
+    if (error instanceof Error) {
+      logger.error(error.message);
+      throw new Error(error.message);
+    }
+    logger.error(error);
+    throw new Error(`${error}`);
+  }
+}
+export { createDNA, updateDNA };
--- src/types/dna.ts
+++ src/types/dna.ts
@@ -5,3 +5,19 @@ export interface IDNAInput {
   kitName: string;
   customerId: string;
 }
+
+export interface IDNAUpdateInput {
+  qcStatus?: "pending" | "approved" | "rejected" | string;
+  labId?: string;
+  statusLog?: StatusLogEntry[];
+}
+
+interface StatusLogEntry {
+  status: string;
+  remark: string;
+  date: string; // ISO 8601 date string
+  concentration: string; // or number, if handled as numeric
+  firstRatio: number;
+  secondRatio: number;
+  extractionLabID: string;
+}

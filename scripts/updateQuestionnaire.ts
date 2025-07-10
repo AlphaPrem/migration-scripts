00f1a1e1ba510402537e6eabf06eb9df8dbac8a8
Prem Goswami <premgoswami2892@gmail.com>
2025-07-10 14:38:56 +0530

    update questionnaire script

--- scripts/updateQuestionnaire.ts
+++ scripts/updateQuestionnaire.ts
@@ -1,7 +1,7 @@
 // src/seedCustomers.ts
 import { PrismaClient } from "@prisma/client";
 import { writeFileSync } from "fs";
-import { questionnaires } from "../data/questionnaire"; // Import the static list of customers
+import { existing_quesitonnaire_barcodes } from "../data/barcodes";
 import logger from "../lib/logger/logger";
 
 const prisma = new PrismaClient();
@@ -9,55 +9,31 @@ const prisma = new PrismaClient();
 async function main() {
   const IDS: string[] = [];
 
-  if (questionnaires.length === 0) {
+  if (existing_quesitonnaire_barcodes.length === 0) {
     console.warn("No customer data supplied ‑ nothing to seed.");
     return;
   }
-
-  /**
-   * `prisma.$transaction` ensures that either
-   * *all* documents are inserted or *none* are.
-   * (Requires MongoDB 4.2+ on a replica‑set or sharded cluster.)
-   */
-
-  const processedQuestionnaires = questionnaires.map(
-    ({ dob, createdAt, updatedAt, Id, ...rest }: any) => {
-      return {
-        ...rest,
-        DOB: new Date(dob).toISOString(),
-        createdAt: new Date(createdAt),
-        version: "3",
-        // updatedAt: new Date(updatedAt),
-      };
-    }
-  );
-
-  logger.info(
-    "Data to be inserted:",
-    processedQuestionnaires.length,
-    "records."
-  );
 
   //   console.log(processedQuestionnaires);
 
   const created = await prisma.$transaction(
     async (tx) => {
-      const results = [];
+      const results: any[] = [];
 
-      for (const data of processedQuestionnaires) {
-        logger.info(`Processing questionnaire for barcode: ${data.kitCode}`);
+      for (const data of existing_quesitonnaire_barcodes) {
+        logger.info(`Processing questionnaire for barcode: ${data}`);
         const updatedRecord = await tx.sampleCollectionData.update({
-          where:{
-            kitCode:data.kitCode
+          where: {
+            id: data,
           },
-          data:{
-            version:"3"
-          }
+          data: {
+            version: "3",
+          },
         });
 
         results.push(updatedRecord);
 
-        logger.info("✅ Questionnaire updated:", );
+        logger.info("✅ Questionnaire updated: ", data);
         IDS.push(updatedRecord.id);
       }
 
@@ -71,18 +47,18 @@ async function main() {
   // 3️⃣  Extract the IDs only.
   const ids = created.map((c: any) => c.id);
 
-  writeFileSync(
-    "./modified/questionnaire/updated_questionnaires.json",
-    JSON.stringify(created, null, 2)
-  );
-  writeFileSync(
-    "./modified/questionnaire/updated_questionnaires_id.json",
-    JSON.stringify(ids, null, 2)
-  );
-  writeFileSync(
-    "./modified/questionnaire/ids_updated_backup.json",
-    JSON.stringify(IDS, null, 2)
-  );
+  // writeFileSync(
+  //   "./modified/questionnaire/updated_questionnaires.json",
+  //   JSON.stringify(created, null, 2)
+  // );
+  // writeFileSync(
+  //   "./modified/questionnaire/updated_questionnaires_id.json",
+  //   JSON.stringify(ids, null, 2)
+  // );
+  // writeFileSync(
+  //   "./modified/questionnaire/ids_updated_backup.json",
+  //   JSON.stringify(IDS, null, 2)
+  // );
 }
 
 main()

34b500bddf0bcea751f9f55811daefe72af7851f
Prem Goswami <premgoswami2892@gmail.com>
2025-07-10 09:54:46 +0530

    refactor 1

---
+++ data/outward.ts
@@ -0,0 +1,202 @@
+export const outwards = [
+  {
+    Shopify_Kit_Id__c: "000415005",
+    CreatedDate: "2024-11-28 12:01:53",
+    LastModifiedDate: "2025-01-21 09:10:06",
+  },
+  {
+    Shopify_Kit_Id__c: "000414845",
+    CreatedDate: "2024-11-29 10:30:09",
+    LastModifiedDate: "2025-01-07 05:53:19",
+  },
+  {
+    Shopify_Kit_Id__c: "000415079",
+    CreatedDate: "2024-12-03 09:15:03",
+    LastModifiedDate: "2024-12-24 08:30:03",
+  },
+  {
+    Shopify_Kit_Id__c: "000018411",
+    CreatedDate: "2024-12-09 03:21:38",
+    LastModifiedDate: "2025-01-03 11:04:10",
+  },
+  {
+    Shopify_Kit_Id__c: "000039660",
+    CreatedDate: "2024-12-11 05:08:07",
+    LastModifiedDate: "2025-01-27 11:07:55",
+  },
+  {
+    Shopify_Kit_Id__c: "000414829",
+    CreatedDate: "2024-12-12 03:32:05",
+    LastModifiedDate: "2025-01-07 05:52:08",
+  },
+  {
+    Shopify_Kit_Id__c: "000414962",
+    CreatedDate: "2024-12-16 05:40:56",
+    LastModifiedDate: "2025-01-21 08:34:48",
+  },
+  {
+    Shopify_Kit_Id__c: "000414443",
+    CreatedDate: "2024-12-16 06:33:06",
+    LastModifiedDate: "2025-01-27 07:33:56",
+  },
+  {
+    Shopify_Kit_Id__c: "000414739",
+    CreatedDate: "2024-12-16 09:16:48",
+    LastModifiedDate: "2025-01-07 05:53:51",
+  },
+  {
+    Shopify_Kit_Id__c: "000414704",
+    CreatedDate: "2024-12-16 10:33:26",
+    LastModifiedDate: "2025-02-19 12:08:39",
+  },
+  {
+    Shopify_Kit_Id__c: "000414825",
+    CreatedDate: "2024-12-16 10:40:21",
+    LastModifiedDate: "2025-01-03 11:00:37",
+  },
+  {
+    Shopify_Kit_Id__c: "000414814",
+    CreatedDate: "2024-12-16 15:13:53",
+    LastModifiedDate: "2025-01-07 05:51:39",
+  },
+  {
+    Shopify_Kit_Id__c: "000414929",
+    CreatedDate: "2024-12-17 03:27:27",
+    LastModifiedDate: "2025-01-04 10:11:33",
+  },
+  {
+    Shopify_Kit_Id__c: "000414930",
+    CreatedDate: "2024-12-17 09:30:13",
+    LastModifiedDate: "2025-01-14 08:37:05",
+  },
+  {
+    Shopify_Kit_Id__c: "000415245",
+    CreatedDate: "2024-12-17 16:13:27",
+    LastModifiedDate: "2025-01-29 07:12:05",
+  },
+  {
+    Shopify_Kit_Id__c: "000415343",
+    CreatedDate: "2024-12-21 03:05:55",
+    LastModifiedDate: "2024-12-21 18:25:26",
+  },
+  {
+    Shopify_Kit_Id__c: "000414791",
+    CreatedDate: "2024-12-21 08:31:41",
+    LastModifiedDate: "2025-01-08 04:46:17",
+  },
+  {
+    Shopify_Kit_Id__c: "000414932",
+    CreatedDate: "2024-12-23 16:47:40",
+    LastModifiedDate: "2025-01-24 06:28:54",
+  },
+  {
+    Shopify_Kit_Id__c: "000414710",
+    CreatedDate: "2024-12-24 06:47:58",
+    LastModifiedDate: "2025-01-07 10:49:58",
+  },
+  {
+    Shopify_Kit_Id__c: "000414690",
+    CreatedDate: "2025-01-04 12:30:12",
+    LastModifiedDate: "2025-02-04 08:16:24",
+  },
+  {
+    Shopify_Kit_Id__c: "000414792",
+    CreatedDate: "2025-01-04 13:43:20",
+    LastModifiedDate: "2025-01-04 14:07:56",
+  },
+  {
+    Shopify_Kit_Id__c: "000039641",
+    CreatedDate: "2025-01-07 03:23:51",
+    LastModifiedDate: "2025-01-07 04:00:26",
+  },
+  {
+    Shopify_Kit_Id__c: "000414407",
+    CreatedDate: "2025-01-09 04:37:00",
+    LastModifiedDate: "2025-01-09 04:50:37",
+  },
+  {
+    Shopify_Kit_Id__c: "000414836",
+    CreatedDate: "2025-01-17 12:57:36",
+    LastModifiedDate: "2025-01-17 16:18:33",
+  },
+  {
+    Shopify_Kit_Id__c: "000007368",
+    CreatedDate: "2025-01-20 10:21:07",
+    LastModifiedDate: "2025-02-15 09:43:52",
+  },
+  {
+    Shopify_Kit_Id__c: "000007363",
+    CreatedDate: "2025-01-23 02:21:14",
+    LastModifiedDate: "2025-01-23 06:08:27",
+  },
+  {
+    Shopify_Kit_Id__c: "000414743",
+    CreatedDate: "2025-01-25 06:59:36",
+    LastModifiedDate: "2025-01-25 07:36:39",
+  },
+  {
+    Shopify_Kit_Id__c: "000007287",
+    CreatedDate: "2025-02-01 10:30:11",
+    LastModifiedDate: "2025-02-04 06:19:14",
+  },
+  {
+    Shopify_Kit_Id__c: "000007309",
+    CreatedDate: "2025-02-02 07:19:06",
+    LastModifiedDate: "2025-02-14 13:37:46",
+  },
+  {
+    Shopify_Kit_Id__c: "000007367",
+    CreatedDate: "2025-02-03 04:41:06",
+    LastModifiedDate: "2025-02-26 13:55:08",
+  },
+  {
+    Shopify_Kit_Id__c: "000011483",
+    CreatedDate: "2025-02-04 06:11:02",
+    LastModifiedDate: "2025-02-04 06:25:21",
+  },
+  {
+    Shopify_Kit_Id__c: "000008670",
+    CreatedDate: "2025-02-05 07:30:19",
+    LastModifiedDate: "2025-02-08 05:58:26",
+  },
+  {
+    Shopify_Kit_Id__c: "000007462",
+    CreatedDate: "2025-02-07 06:09:04",
+    LastModifiedDate: "2025-02-07 06:18:56",
+  },
+  {
+    Shopify_Kit_Id__c: "000007339",
+    CreatedDate: "2025-02-17 13:33:58",
+    LastModifiedDate: "2025-02-17 14:00:27",
+  },
+  {
+    Shopify_Kit_Id__c: "000011387",
+    CreatedDate: "2025-02-17 13:56:02",
+    LastModifiedDate: "2025-02-17 14:22:08",
+  },
+  {
+    Shopify_Kit_Id__c: "000007034",
+    CreatedDate: "2025-02-19 08:30:11",
+    LastModifiedDate: "2025-02-20 19:33:11",
+  },
+  {
+    Shopify_Kit_Id__c: "000007310",
+    CreatedDate: "2025-02-20 03:35:08",
+    LastModifiedDate: "2025-02-20 18:47:57",
+  },
+  {
+    Shopify_Kit_Id__c: "000063630",
+    CreatedDate: "2025-02-21 01:42:38",
+    LastModifiedDate: "2025-02-21 04:59:50",
+  },
+  {
+    Shopify_Kit_Id__c: "000007028",
+    CreatedDate: "2025-02-26 14:20:08",
+    LastModifiedDate: "2025-02-26 14:34:47",
+  },
+  {
+    Shopify_Kit_Id__c: "000007029",
+    CreatedDate: "2025-02-27 10:38:23",
+    LastModifiedDate: "2025-02-27 11:39:46",
+  },
+];
---
+++ scripts/updateOutward.ts
@@ -0,0 +1,96 @@
+// src/seedCustomers.ts
+import { PrismaClient } from "@prisma/client";
+import { writeFileSync } from "fs";
+import { outwards } from "../data/outward";
+import logger from "../lib/logger/logger";
+
+const prisma = new PrismaClient();
+
+interface OutwardDataFormat {
+  Shopify_Kit_Id__c: string;
+  CreatedDate: string;
+  LastModifiedDate: string;
+}
+
+async function main() {
+  if (outwards.length === 0) {
+    console.warn("No customer data supplied ‑ nothing to seed.");
+    return;
+  }
+
+  let validate: boolean = true;
+
+  outwards.forEach((data: OutwardDataFormat) => {
+    if (
+      !data.Shopify_Kit_Id__c ||
+      data.CreatedDate === undefined ||
+      data.LastModifiedDate === undefined
+    ) {
+      logger.error("Missing BARCODE__c in data:", data);
+      validate = false;
+    }
+  });
+
+  if (!validate) {
+    logger.error(
+      "Validation failed: Some data is missing BARCODE__c, createdAt, or updatedAt."
+    );
+    return;
+  }
+
+  /**
+   * `prisma.$transaction` ensures that either
+   * *all* documents are inserted or *none* are.
+   * (Requires MongoDB 4.2+ on a replica‑set or sharded cluster.)
+   */
+  const created = await prisma.$transaction(async (tx) =>
+    Promise.all(
+      outwards.map(async (data: OutwardDataFormat) => {
+        const inventory = await tx.inventory.findUnique({
+          where: { barcode: data.Shopify_Kit_Id__c },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!inventory) {
+          logger.warn(
+            `No inventory found for barcode: ${data.Shopify_Kit_Id__c}`
+          );
+          return null; // Skip if no inventory found
+        }
+
+        const updatedInventory = await tx.outward.updateMany({
+          where: { inventoryId: inventory.id },
+          data: {
+            createdAt: new Date(data.CreatedDate),
+            updatedAt: new Date(data.LastModifiedDate),
+          },
+        });
+
+        return updatedInventory;
+      })
+    )
+  );
+
+  // 3️⃣  Extract the IDs only.
+  const ids = created.map((c: any) => c.id);
+
+  writeFileSync(
+    "./modified/modified_outward.json",
+    JSON.stringify(created, null, 2)
+  );
+  writeFileSync(
+    "./modified/modified_outward_id.json",
+    JSON.stringify(ids, null, 2)
+  );
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Seeding failed:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
